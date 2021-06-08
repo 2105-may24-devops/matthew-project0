ea430fe49e9ff01b6ced9fa2b54218244f5f6498
matthew-cradit <matthew.cradit@revature.net>
2021-06-08 11:25:51 -0500

    test for venv enviroment

--- testing/test_project0.sh
+++ testing/test_project0.sh
@@ -2,7 +2,19 @@
 
 #This file is for testing project 0 
 
+py_interpreter_before=$("python --version")
+
+#start venv
 source ~/matthew-project0-production/venv/bin/activate
 
 #verify python interpreter version 
-py_interpreter=$("python --verson")
+py_interpreter_after=$("python --verson")
+
+if [[ $py_interpreter_before -ne $py_interpreter_after ]]; then
+	result="$py_interpreter_after : venv is correct"
+	echo $result
+else
+	result="Test 1 : Fail incorrect interpreter"
+	echo $result && exit 1
+fi
+
