84e8b34954663af233c84fb953dab4e8ffdb379f
matthew-cradit <matthew.cradit@revature.net>
2021-06-08 23:26:56 -0500

    added new test

--- testing/test_project0.sh
+++ testing/test_project0.sh
@@ -5,9 +5,10 @@
 ex_path=~/matthew-project0-production
 test_path=~/project-data
 
-
+#start venv
 source "$ex_path/venv/bin/activate"
 
+#test1 make sure /project-data isn't present
 if [[ -d $test_path ]] ; then
 	         
 		echo "error: test failed dir present" > error_logs.txt && exit 1
@@ -15,12 +16,22 @@ if [[ -d $test_path ]] ; then
 		echo "test 1: pass "
 	
 fi
-
+#run non interative program 
 $ex_path/venv/bin/python3 $ex_path/main.py collection.txt 0
 
+#test2 create /project-data/
 if [[ -d $test_path ]] ; then
 	echo "Test 2 pass dir created"
 else
-	echo "error test 2 fail" > error_logs.txt && exit 1
+	echo "error test 2 fail" >> error_logs.txt && exit 1
 fi
+
+#test3 check if collection_file.txt is created
+if [[ -e "$test_path/collection_file.txt" ]] ; then
+	echo "test3: passed file created"
+else
+	echo "error: test3 failed" >> error_logs.txt && exit 1
+fi 
+
+exit 0 
 
