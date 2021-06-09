effde4aeaaa2f059710844f168b7b4ca7c15415d
matthew-cradit <matthew.cradit@revature.net>
2021-06-08 23:17:00 -0500

    adding bash test

--- testing/test_project0.sh
+++ testing/test_project0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #this file is for testing project 0
-pwd
+
 
 ex_path=~/matthew-project0-production
 test_path=~/project-data
@@ -10,7 +10,17 @@ source "$ex_path/venv/bin/activate"
 
 if [[ -d $test_path ]] ; then
 	         
-		[[ -d $test_path ]] 2> error_logs.txt && exit 1
+		echo "error: test failed dir present" > error_logs.txt && exit 1
+	else
+		echo "test 1: pass "
 	
+fi
+
+$ex_path/venv/bin/python3 $ex_path/main.py collection.txt 0
+
+if [[ -d $test_path ]] ; then
+	echo "Test 2 pass dir created"
+else
+	echo "error test 2 fail" > error_logs.txt && exit 1
 fi
 
